ba872cf4befa9c96d33bcaea5e357f7ccc905d44
Petr Messner <petr.messner@gmail.com>
2021-03-18 11:59:42 +0100

    WIP threads: add mutex around database operations

--- bloom/database.py
+++ bloom/database.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 import sqlite3
+from threading import Lock
 from time import time
 import zlib
 
@@ -25,30 +26,34 @@ class Database:
 
     def __init__(self, conn):
         self._conn = conn
+        self._lock = Lock()
 
     def close(self):
-        self._conn.close()
-        self._conn = None
+        with self._lock:
+            self._conn.close()
+            self._conn = None
 
     def init(self):
-        cur = self._conn.cursor()
-        cur.execute('''
-            CREATE TABLE IF NOT EXISTS bloom_files_v2 (
-                key TEXT PRIMARY KEY,
-                path TEXT,
-                created INTEGER,
-                array BLOB
-            )
-        ''')
-        self._conn.rollback()
+        with self._lock:
+            cur = self._conn.cursor()
+            cur.execute('''
+                CREATE TABLE IF NOT EXISTS bloom_files_v2 (
+                    key TEXT PRIMARY KEY,
+                    path TEXT,
+                    created INTEGER,
+                    array BLOB
+                )
+            ''')
+            self._conn.rollback()
 
     def get_file_array(self, path, size, mtime, version, sample_sizes):
         sample_sizes_str = ','.join(str(i) for i in sample_sizes)
         key = f"{path}:{size}:{mtime}:{version}:{sample_sizes_str}"
-        cur = self._conn.cursor()
-        cur.execute('SELECT array FROM bloom_files_v2 WHERE key=?', (key, ))
-        row = cur.fetchone()
-        self._conn.commit()
+        with self._lock:
+            cur = self._conn.cursor()
+            cur.execute('SELECT array FROM bloom_files_v2 WHERE key=?', (key, ))
+            row = cur.fetchone()
+            self._conn.commit()
         return zlib.decompress(row[0]) if row else None
 
     def set_file_array(self, path, size, mtime, version, sample_sizes, array):
@@ -56,15 +61,16 @@ class Database:
         sample_sizes_str = ','.join(str(i) for i in sample_sizes)
         key = f"{path}:{size}:{mtime}:{version}:{sample_sizes_str}"
         now = int(time())
-        cur = self._conn.cursor()
-        # The upsert syntax works in sqlite since 3.24.0, but it seems some Python installations have older version
-        #cur.execute('''
-        #    INSERT INTO bloom_files_v1 (key, created, array) VALUES (?, ?, ?)
-        #    ON CONFLICT (key) DO UPDATE SET created=?, array=?
-        #''', (key, now, array, now, array))
-        # So let's do DELETE + INSERT instead :)
-        cur.execute('DELETE FROM bloom_files_v2 WHERE path=?', (str(path), ))
-        cur.execute('''
-            INSERT INTO bloom_files_v2 (key, path, created, array) VALUES (?, ?, ?, ?)
-        ''', (key, str(path), now, zlib.compress(array)))
-        self._conn.commit()
+        with self._lock:
+            cur = self._conn.cursor()
+            # The upsert syntax works in sqlite since 3.24.0, but it seems some Python installations have older version
+            #cur.execute('''
+            #    INSERT INTO bloom_files_v1 (key, created, array) VALUES (?, ?, ?)
+            #    ON CONFLICT (key) DO UPDATE SET created=?, array=?
+            #''', (key, now, array, now, array))
+            # So let's do DELETE + INSERT instead :)
+            cur.execute('DELETE FROM bloom_files_v2 WHERE path=?', (str(path), ))
+            cur.execute('''
+                INSERT INTO bloom_files_v2 (key, path, created, array) VALUES (?, ?, ?, ?)
+            ''', (key, str(path), now, zlib.compress(array)))
+            self._conn.commit()
